ec327d05090416ab9708eecd9a25ea4c03ba03e4
Satyaki_Abhijit <as2688@it.jgec.ac.in>
2024-12-06 17:05:28 +0530

    Initial commit for Flask app

--- main_app.py
+++ main_app.py
@@ -1,42 +1,40 @@
-# Library imports
 import numpy as np
-import streamlit as st
-import cv2
+from flask import Flask, request, jsonify
 from keras.models import load_model
-import tensorflow as tf
+from PIL import Image
+import io
 
-# Loading the Model
+app = Flask(__name__)
+
+# Load your pre-trained model
 model = load_model('plant_disease_model.h5')
-                    
-# Name of Classes
-CLASS_NAMES = ('Tomato-Bacterial_spot', 'Potato-Barly blight', 'Corn-Common_rust')
 
-# Setting Title of App
-st.title("Plant Disease Detection")
-st.markdown("Upload an image of the plant leaf")
+# Define class names for predictions
+CLASS_NAMES = ['Tomato-Bacterial_spot', 'Potato-Barly blight', 'Corn-Common_rust']
 
-# Uploading the dog image
-plant_image = st.file_uploader("Choose an image...", type = "jpg")
-submit = st.button('predict Disease')
 
-# On predict button click
-if submit:
-    if plant_image is not None:
-        # Convert the file to an opencv image.
-        file_bytes = np.asarray (bytearray(plant_image.read()), dtype = np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        
-        # Displaying the image
-        st.image(opencv_image, channels="BGR")
-        st.write(opencv_image.shape)
-        
-        # Resizing the image
-        opencv_image = cv2.resize(opencv_image, (256, 256))
-        
-        # Convert image to 4 Dimension
-        opencv_image.shape = (1, 256, 256, 3)
-        
-        #Make Prediction
-        Y_pred = model.predict(opencv_image)
-        result = CLASS_NAMES[np.argmax(Y_pred)]
-        st.title(str("This is "+result.split('-')[0]+ " leaf with " +  result.split('-')[1]))
+# Prediction endpoint
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+
+    # Read the image in the file
+    img = Image.open(file.stream)
+
+    # Resize and preprocess the image
+    img = img.resize((256, 256))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # Make a prediction
+    prediction = model.predict(img_array)
+    result = CLASS_NAMES[np.argmax(prediction)]
+
+    return jsonify({'result': result})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
